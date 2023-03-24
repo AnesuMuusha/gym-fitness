83a63a145d1f699972a1dbb0a57667380ef5deee
AnesuMuusha <anesumuusha@gmail.com>
2023-03-24 09:15:02 +0200

    Added relevant content in component Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,71 @@
-import React from 'react'
+import { useState } from "react";
 
 function Contact() {
-  return (<>
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-<div>Contact</div>
-<div>Contact</div>
-<div>Contact</div>
-<div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    <div>Contact</div>
-    </>)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // handle form submission here
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="bg-gray-200 p-6 rounded-lg">
+      <h2 className="text-2xl font-bold text-gray-800 mb-6">Contact Us</h2>
+      <div className="mb-4">
+        <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div className="mb-6">
+        <label htmlFor="message" className="block text-gray-700 font-bold mb-2">
+          Message
+        </label>
+        <textarea
+          id="message"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Enter your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+      </div>
+      <button
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-600 p-1 border rounded b text-white cursor-pointer"
+      >
+        Send
+      </button>
+    </form>
+  );
 }
 
 export default Contact;
