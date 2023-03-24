9587f285abb3fd14e7754c188e07b717d390959c
AnesuMuusha <anesumuusha@gmail.com>
2023-03-24 09:20:53 +0200

    added data in Post data

---
+++ src/components/PostData.js
@@ -0,0 +1,47 @@
+export const posts = [
+    {
+        title: "Chest",
+        description:
+          "Chest workouts typically involve exercises such as bench press, push-ups, and chest flies. These exercises target the pectoral muscles, which are the main muscles in the chest.",
+        path: "xamarin-forms-guide",
+      },
+      {
+        title: "Back",
+        description:
+          "Deadlifts: Stand with a weighted barbell on the ground and lift it up to hip level. Rows: Stand with a weight in each hand and pull the weights up towards your chest while squeezing your shoulder blades together.",
+        path: "xamarin-forms-guide",
+      },
+      {
+        title: "Shoulders",
+        description:
+          "Overhead press - compound exercise that targets the shoulders, triceps, and upper back. Lateral raise - isolation exercise that targets the medial deltoids.",
+        path: "xamarin-forms-guide",
+      },
+      {
+        title: "Arms (biceps and triceps)",
+        description:
+          "Bicep curls - isolation exercise that targets the biceps. Tricep extensions - isolation exercise that targets the triceps.",
+        path: "xamarin-forms-guide",
+      },
+      {
+        title: "Abs (abdominals)",
+        description:
+          "Planks - isometric exercise that targets the entire core. Crunches - isolation exercise that targets the rectus abdominis. Russian twists - isolation exercise that targets the obliques.",
+        path: "xamarin-forms-guide",
+      },
+        
+      {
+        title: "Legs (quadriceps, hamstrings, calves)",
+        description:
+          "Squats - compound exercise that targets the quadriceps, hamstrings, and glutes. Lunges - compound exercise that targets the quadriceps, hamstrings, and glutes.",
+        path: "xamarin-forms-guide",
+      },
+        
+      {
+        title: "Glutes (buttocks)",
+        description:
+          "Squats - compound exercise that targets the glutes, quads, and hamstrings. Lunges - compound exercise that targets the glutes, quads, and hamstrings.",
+        path: "xamarin-forms-guide",
+      },
+        
+    ]
