81887d94a6f16ef9f70112de7b5eb35230988d87
AnesuMuusha <anesumuusha@gmail.com>
2023-03-24 09:15:57 +0200

    changed gym home page style

--- src/components/GymHomepage.js
+++ src/components/GymHomepage.js
@@ -10,7 +10,7 @@ function GymHomepage() {
       <div className="flex flex-col m-5 w-5/5">
         <img src={gymImage} alt="Gym" className="" />
         <div className="flex flex-col items-center">
-          <h1 className="text-bold font-bold text-lg  mt-2">Welcome to Fit Gym</h1>
+          <h1 className="font-serif text-bold font-bold text-lg  mt-2">Welcome to Fit Gym</h1>
           <p className="my-4 px-3 text-gray-800">
             We are dedicated to providing a
             world-class fitness experience for everyone, from beginners to
@@ -18,7 +18,7 @@ function GymHomepage() {
             looking to lose weight, build muscle, or simply improve your overall
             health and wellness, we have everything you need to get started.{" "}
           </p>
-          <button className="bg-gray-600 p-1 border rounded b text-white cursor-pointer">
+          <button className="bg-gray-800 hover:bg-gray-600 p-1 border rounded b text-white cursor-pointer">
             Learn More
           </button>
         </div>
