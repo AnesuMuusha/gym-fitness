2b61f334a3a4a5bed30b7e563d57f01528cb2d88
AnesuMuusha <anesumuusha@gmail.com>
2023-03-28 07:48:48 +0200

    styled the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex flex-col text-sm text-center md:text-3xl'>
-    <footer className='m-4 p-2 bg-gray-200 text-gray-500'>@copyright</footer>
+    <div className='flex flex-col text-sm text-center md:text-2xl '>
+    <footer className='m-4 p-2 bg-gray-100 text-gray-500'>@copyright</footer>
     </div>
     )
 }
