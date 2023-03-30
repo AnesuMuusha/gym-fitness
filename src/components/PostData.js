30ccca887e3b8828aa4235523858a8b12dda3509
AnesuMuusha <anesumuusha@gmail.com>
2023-03-30 14:59:44 +0200

    images update

--- src/components/PostData.js
+++ src/components/PostData.js
@@ -1,54 +1,55 @@
-import imageAbs from "./images/abs.jpg";
-import imageBack from "./images/back.jpg";
-import imageChest from "./images/back.jpg";
-import imageShoulders from "./images/shoulders.jpg";
-import imageArms from "./images/arm.jpg";
-import imageLeg from "./images/leg.jpg";
-import imageGlutes from "./images/back.jpg"
+const imageAbs = require ("./images/abs.jpg");
+const imageBack = require ("./images/back.jpg");
+const imageChest = require("./images/chest.jpg");
+const imageShoulders = require("./images/shoulders.jpg");
+const imageArms = require("./images/arm.jpg");
+const imageLeg = require("./images/leg.jpg");
+const imageGlutes = require("./images/glutes.jpg");
 export const posts = [
     {
         title: "Chest",
         description:
           "Chest workouts typically involve exercises such as bench press, push-ups, and chest flies. These exercises target the pectoral muscles, which are the main muscles in the chest.",
-          imagePath: {imageChest},
+          imagePath: imageChest,
       },
       {
         title: "Back",
         description:
           "Dead lifts: Stand with a weighted barbell on the ground and lift it up to hip level. Rows: Stand with a weight in each hand and pull the weights up towards your chest while squeezing your shoulder blades together.",
-          imagePath: {imageBack},
+          imagePath: imageBack,
       },
       {
         title: "Shoulders",
         description:
           "Overhead press - compound exercise that targets the shoulders, triceps, and upper back. Lateral raise - isolation exercise that targets the medial deltoids.",
-          imagePath: {imageShoulders},
+          imagePath: imageShoulders,
       },
       {
         title: "Arms (biceps and triceps)",
         description:
           "Bicep curls - isolation exercise that targets the biceps. Tricep extensions - isolation exercise that targets the triceps.",
-          imagePath: {imageArms},
+          imagePath: imageArms,
       },
       {
         title: "Abs (abdominals)",
         description:
           "Planks - isometric exercise that targets the entire core. Crunches - isolation exercise that targets the rectus abdominis. Russian twists - isolation exercise that targets the obliques.",
-          imagePath: {imageAbs},
+          imagePath: imageAbs,
       },
         
       {
         title: "Legs (quadriceps, hamstrings, calves)",
         description:
           "Squats - compound exercise that targets the quadriceps, hamstrings, and glutes. Lunges - compound exercise that targets the quadriceps, hamstrings, and glutes.",
-          imagePath: {imageLeg},
+          imagePath: imageLeg
+          ,
       },
         
       {
         title: "Glutes (buttocks)",
         description:
           "Squats - compound exercise that targets the glutes, quads, and hamstrings. Lunges - compound exercise that targets the glutes, quads, and hamstrings.",
-          imagePath: {imageGlutes},
+          imagePath: imageGlutes,
       },
         
     ]
