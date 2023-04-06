45f9d9df6c45d95e8dafe625c1da0cdcf9d97144
AnesuMuusha <anesumuusha@gmail.com>
2023-04-06 11:44:27 +0200

    used arrow function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Footer() {
+const Footer=()=> {
   return (
     <div className='flex flex-col text-sm text-center md:text-2xl '>
     <footer className='m-4 p-2 bg-gray-100 text-gray-500'>@copyright</footer>
