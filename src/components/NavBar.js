cdb6610e13112242d4a18eed01d44c5f60dcb280
AnesuMuusha <anesumuusha@gmail.com>
2023-04-06 11:46:19 +0200

    added meals to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,22 @@
 import React from 'react';
-function NavBar() {
+const NavBar=()=> {
   return (
  
 <header className='fixed flex text-sm bg-gray-100 md:text-3xl text-gray-500 w-full top-0'>
    <a href='#Home'>
     <div  className='ml-8'>Home</div>
     </a>
-   <a href='#Book'>
+    {/* <a href='#Schedule'>
+    <div className='ml-2'>Schedule</div>
+    </a> */}
+   {/* <a href='#Book'>
    <div className='ml-2'>Book</div>
-   </a>    
+   </a>     */}
    <a href='#Explore'>
     <div className='ml-2'>Explore</div>
+    </a>
+    <a href='#meals'>
+    <div className='ml-2'>Meals</div>
     </a>
     <a href='#Contact'>
     <div className='ml-2'>Contact</div>
